dd43860be54726eff9e3e45ff888f99933073d2f
Adimaha1 <aditya.mahara@gmail.com>
2023-06-18 10:47:18 -0400

    Added encryption test

--- DataJob/Program.cs
+++ DataJob/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+EncryptionTest.Class1.TestMethod("Encryption Test Called");
---
+++ EncryptionTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+
+namespace EncryptionTest
+{
+    public class Class1
+    {
+        public static void TestMethod (string strTest)
+        {
+            Console.WriteLine(strTest);
+        }
+    }
+}
